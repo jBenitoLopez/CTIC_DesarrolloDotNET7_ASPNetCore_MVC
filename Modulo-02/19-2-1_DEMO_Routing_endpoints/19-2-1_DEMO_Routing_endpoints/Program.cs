245f7743c39da4d8635db1624839eacac8b8d265
jBenitoLopez <lopez.jbenito@gmail.com>
2023-05-05 16:34:37 +0200

    mod: 19_2_1_DEMO_Routing_endpoints base Program.cs

--- Modulo-02/19-2-1_DEMO_Routing_endpoints/19-2-1_DEMO_Routing_endpoints/Program.cs
+++ Modulo-02/19-2-1_DEMO_Routing_endpoints/19-2-1_DEMO_Routing_endpoints/Program.cs
@@ -1,6 +1,44 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+
+
+
+#region one
+//app.UseRouting(); // es añadido automaticamente por la API
+// middleware
+//app.Use(async (ctx, next) => 
+//{
+//    var selectedEndpoint = ctx.GetEndpoint();
+
+//    if (selectedEndpoint?.DisplayName == "Hello endpoint!")
+//    {
+//        var name = ctx.GetRouteValue("name")?.ToString();
+//        if("Jon".Equals(name, StringComparison.CurrentCultureIgnoreCase))
+//        {
+//            ctx.Request.RouteValues["name"] = "John";
+//        }
+//    }
+//    await next(ctx);
+//});
+
+// UseEndpoints es añadido automaticamente por la API
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapGet("/hello/{name}", (string name) =>
+//    {
+//        return $"Hello {name}!";
+//    }).WithDisplayName("Hello endpoint!");
+//});
+#endregion one
+
+#region two
+    app.MapGet("/hello/{name}", (string name) =>
+    {
+        return $"Hello {name}!";
+    }).WithDisplayName("Hello endpoint!");
+#endregion two
 
 app.Run();
