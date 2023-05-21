1484d5b7f587912741b3a3efc250077ab9910761
jBenitoLopez <lopez.jbenito@gmail.com>
2023-05-21 17:09:29 +0200

    add: 05 - DEMO: Puesta en marcha y ejemplos simples

---
+++ Modulo-03/MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController:Controller
+    {
+
+        public IActionResult Index() =>  Content("Home/Index (Default Controller & default View)");
+    }
+}
---
+++ Modulo-03/MVC/MVC/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class TestController : Controller
+    {
+
+        // Ruta por defecto: {controler}/{action}/{id?}
+
+        public IActionResult Index()
+        {
+            return Content("Hello from INDEX");
+        }
+
+
+        // GET: https://localhost:7077/test/hello/jhon?age=33
+        public IActionResult Hello(string id, int age)
+        {
+            var name = id ?? "name";
+            return Content($"Hello, {name}! You are {age} years old.");
+        }
+    }
+}
