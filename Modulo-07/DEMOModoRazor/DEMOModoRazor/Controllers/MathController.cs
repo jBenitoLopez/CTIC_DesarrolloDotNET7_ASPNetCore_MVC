888c435fc6cca06bafc30ba0bae839bf24860860
jBenitoLopez <lopez.jbenito@gmail.com>
2023-06-14 20:24:08 +0200

    add: 05 - DEMO: Los dos modos de Razor

---
+++ Modulo-07/DEMOModoRazor/DEMOModoRazor/Controllers/MathController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DEMOModoRazor.Controllers
+{
+    public class MathController : Controller
+    {
+        // https://localhost:7201/math/mul/22
+        [Route("/math/mul/{n}")]
+        public IActionResult Multiplicacion(int n)
+        {
+            return View(n);
+        }
+    }
+}
---
+++ Modulo-07/DEMOModoRazor/DEMOModoRazor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DEMOModoRazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
