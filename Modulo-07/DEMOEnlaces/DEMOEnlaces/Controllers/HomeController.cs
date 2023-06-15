8aba494c2af93748c115653c728cce655049c1e1
jBenitoLopez <lopez.jbenito@gmail.com>
2023-06-15 16:39:29 +0200

    add: 09.2 - DEMO: Generación de enlaces

---
+++ Modulo-07/DEMOEnlaces/DEMOEnlaces/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using DEMOEnlaces.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DEMOEnlaces.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("/people/say")]
+        public IActionResult Say(string message, string to)
+        {
+            return Content($"{to} say {message}!!!");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
