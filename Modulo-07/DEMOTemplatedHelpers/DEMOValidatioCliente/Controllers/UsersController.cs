8de5b6233e0734893e892271612e6aa876326245
jBenitoLopez <lopez.jbenito@gmail.com>
2023-06-17 11:59:30 +0200

    add: 09.11 - DEMO: Templated helpers

---
+++ Modulo-07/DEMOTemplatedHelpers/DEMOValidatioCliente/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using DEMOValidatioCliente.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DEMOValidatioCliente.Controllers
+{
+    public class UsersController : Controller
+    {
+        // https://localhost:7057/users/create
+        public IActionResult Create()
+        {
+            return View(new CreateUserViewModel() { Username = "Pepito", FullName = "Pepito Grillo", Email = "pepito@mail.loc", Password = "asdfasdf", RePassword = "asdfasdf", Enabled = true });
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateUserViewModel vm)
+        {
+            if (!ModelState.IsValid) return View(vm);
+
+            // TODO: Salvar los datos del usuario
+
+            return View("Details", vm);
+        }
+
+        public IActionResult Details(CreateUserViewModel vm)
+        {
+            return View(vm);
+        }
+    }
+}
---
+++ Modulo-07/DEMOTemplatedHelpers/DEMOValidatioCliente/Models/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DEMOValidatioCliente.Models.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        [Required, StringLength(20, MinimumLength = 3)]
+        public string Username { get; set; }
+
+        [StringLength(50)]
+        public string FullName { get; set; }
+
+        [EmailAddress, StringLength(100)]
+        public string Email { get; set; }
+
+        [Required, StringLength(29, MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, StringLength(29, MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string RePassword { get; set; }
+
+        public bool Enabled { get; set; }
+    }
+}
