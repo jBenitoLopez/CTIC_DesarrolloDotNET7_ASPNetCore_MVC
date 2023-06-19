dee297384a1b66ad12c985775af61b94cd17e3f2
jBenitoLopez <lopez.jbenito@gmail.com>
2023-06-19 20:38:28 +0200

    add: 09.1 - DEMO: El filtro [APIController]

---
+++ Modulo-08/DEMOApiController/DEMOApiController/Controllers/FriendsController.cs
@@ -0,0 +1,18 @@
+﻿using DEMOApiController.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DEMOApiController.Controllers
+{
+    [ApiController]
+    public class FriendsController : ControllerBase
+    {
+        [HttpPost("/friends/create")]
+        public ActionResult<Friend> Create(Friend f)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            return f;
+        }
+    }
+}
---
+++ Modulo-08/DEMOApiController/DEMOApiController/Models/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DEMOApiController.Models
+{
+    public class Friend
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1, 120)]
+        public int Age { get; set; }
+    }
+}
